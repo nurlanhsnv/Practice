a39e08e844f1a1b38ffd118a5785780b0b944f89
nurlanhsnv <161680141+nurlanhsnv@users.noreply.github.com>
2024-10-24 19:32:50 +0400

    gradle falan

---
+++ src/main/kotlin/com/example/springbootpractice/SpringBootPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springbootpractice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootPracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootPracticeApplication>(*args)
+}
